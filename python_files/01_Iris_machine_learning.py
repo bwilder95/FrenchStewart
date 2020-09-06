bd1e8f113bac37c6777c623588e2e53c1e737e5f
bwilder95 <brentonwilder1995@gmail.com>
2020-09-06 15:09:26 -0700

    clf plot and pipeline added

--- python_files/01_Iris_machine_learning.py
+++ python_files/01_Iris_machine_learning.py
@@ -6,6 +6,13 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+from sklearn import tree
+from sklearn.datasets import make_classification
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import LabelEncoder, Normalizer, StandardScaler
+from sklearn.svm import SVC
 
 # Read iris data and add appropriate column headers
 Iris = pd.read_csv("data/iris.data", header=None)
@@ -18,6 +25,9 @@ Iris.columns = [
 ]
 
 # Calculate basic statistics of the dataset
+print("*" * 80)
+print("Statistics")
+print("*" * 80)
 print(Iris.describe(include="all"))
 
 # Figure 1 - Violin plot comparing petal length (cm)
@@ -65,4 +75,74 @@ cx = sns.scatterplot(
     linewidth=0,
     data=Iris,
 )
+
+# Analyze and build models using scikit-learn
+# Print original dataset
+print("*" * 80)
+print("Original Dataset")
+print("*" * 80)
+print(Iris)
+
+# DataFrame to numpy arrays
+x = Iris[
+    [
+        "sepal length (cm)",
+        "sepal width (cm)",
+        "petal length (cm)",
+        "petal width (cm)",
+    ]
+].values
+y = Iris["class"].values
+
+# Use LabelEncoder to make categorical variables numeric
+lb_make = LabelEncoder()
+y = lb_make.fit_transform(y)
+print("*" * 80)
+print("LabelEncoder changing flower type to 0,1,or 2")
+print("*" * 80)
+print(y)
+
+# Normalize "x" (predictors)
+normalizer = Normalizer().fit(x)
+x_norm = normalizer.transform(x)
+print("*" * 80)
+print("Normalize data to range 0 to 1")
+print("*" * 80)
+print(x_norm)
+
+# Random forest classifier
+rf = RandomForestClassifier(random_state=1234)
+rf.fit(x_norm, y)
+prediction = rf.predict(x_norm)
+probability = rf.predict_proba(x_norm)
+print("*" * 80)
+print("Random Forest Model Predictions")
+print("*" * 80)
+print(f"Probability: {probability}")
+print(f"Predictions: {prediction}")
+
+# DecisionTreeClassifer
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(x_norm, y)
+clf_pred = clf.predict(x_norm)
+clf_prob = clf.predict_proba(x_norm)
+plt.figure()
+tree.plot_tree(clf, filled=True)
+print("*" * 80)
+print("Decision Tree Model Predictions")
+print("*" * 80)
+print(f"Probability: {clf_prob}")
+print(f"Predictions: {clf_pred}")
+
+# Creating pipeline for test set with C-Support Vector Classification
+x_norm, y = make_classification(random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(x_norm, y, random_state=0)
+pipe = Pipeline([("scaler", StandardScaler()), ("svc", SVC())])
+pipe.fit(x_train, y_train)
+print("*" * 80)
+print("Pipeline prediction score using SVC")
+print("*" * 80)
+print(pipe.score(x_test, y_test))
+
+# Plot all figures
 plt.show()
