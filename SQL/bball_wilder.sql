6dd4605be2c6a4d7b6f3edcadd923635fc9ef174
Brent Wilder <56372982+bwilder95@users.noreply.github.com>
2020-09-27 23:25:45 -0700

    very close

--- SQL/bball_wilder.sql
+++ SQL/bball_wilder.sql
@@ -13,33 +13,37 @@ select *
 	limit 0,20;
 
 -- Create new table with everyone's hits and atbats
+DROP TABLE IF EXISTS batting_avg_hist3;
 CREATE TABLE batting_avg_hist3
   AS (SELECT batter, atBat, Hit FROM batter_counts)
   
  -- Display table of historic batting percentage for all batters
- select batter, avg(Hit/atBat) as batPercent
+ select batter, (SUM(Hit) / SUM(atBat)) as batPercent
  	from batting_avg_hist3 
  	group by batter
  	limit 0,20;
 
 -- Create new table for annual statistics by joining to game_id
+DROP TABLE IF EXISTS batting_avg_annual;
 create table batting_avg_annual
 	as (select batter_counts.batter,batter_counts.Hit,batter_counts.atBat,game.local_date
 	from batter_counts 
 	inner join game on batter_counts.game_id=game.game_id)
 	
  -- Display table of annual average bat percent for each batter
- select batter, avg(Hit/atBat) as batPerrcent,year(local_date) as year
+ select batter, (SUM(Hit) / SUM(atBat)) as batPerrcent,year(local_date) as year
  	from batting_avg_annual
  	group by batter,year
  	limit 0,20;
 
--- Create rolling window table
-select a.local_date,a.batter,
-round((select avg(b.Hit/b.atBat)
-	from batting_avg_annual as b
-	where datediff(a.local_date,b.local_date) between 0 and 100
-	),7) as '100dayMovingAVG'
-	from batting_avg_annual as a
-	order by a.batter,a.local_date desc
-	limit 0,20;
+ 
+ -- Create rolling window table
+SELECT a.local_date,a.batter,
+(SELECT (SUM(b.Hit)/(SUM(b.atBat)))
+FROM (select *
+	  from batting_avg_annual
+	  group by batter) as b
+WHERE DATEDIFF(a.local_date,b.local_date) BETWEEN 0 AND 100) AS battingavg100day
+FROM batting_avg_annual AS a
+ORDER BY a.batter,a.local_date DESC
+LIMIT 0,20;
