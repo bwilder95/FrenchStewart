6cf3de0af1c169ed511a51f76c208c1fcd157700
bwilder95 <brentonwilder1995@gmail.com>
2020-09-29 17:23:05 -0700

    Getting there

--- python_files/02_Sparky_bball.py
+++ python_files/02_Sparky_bball.py
@@ -3,6 +3,44 @@
 # September 28th, 2020
 
 # Import libraries
-import pandas as pd
+import sys
+import tempfile
+import requests
+from pyspark import StorageLevel
+from pyspark.sql import SparkSession
+
+# Set main
+def main():
+    # Initialize Spark
+    spark = SparkSession.builder.master("local[*]").getOrCreate()
+    database = "baseball"
+    port = "3307"
+    user = "root"
+    password = "password123"
+    # Load 'batter_counts' table
+    batter_counts = spark.read.format("jdbc").options(
+        url=f"jdbc:mysql://localhost:{port}/{database}",
+        driver="com.mysql.cj.jdbc.Driver",
+        dbtable="batter_counts",
+        user=user,
+        password=password).load()
+
+    #batter_counts.show()
+    # Load 'game' table
+    game = spark.read.format("jdbc").options(
+        url=f"jdbc:mysql://localhost:{port}/{database}?zeroDateTimeBehavior=convertToNull",
+        driver="com.mysql.cj.jdbc.Driver",
+        dbtable="game",
+        user=user,
+        password=password).load()
+
+    #game.show()
+
+    # Join tables as "BballCombine"
+    BballCombine = game.join(batter_counts, on=['game_id'], how='inner')
+
+
+if __name__ == "__main__":
+    sys.exit(main())
 
 
