7533651144cf320f15c70d91117f2792d2567249
bwilder95 <brentonwilder1995@gmail.com>
2020-09-29 18:10:37 -0700

    not allowed non-eq predicate

--- python_files/02_Sparky_bball.py
+++ python_files/02_Sparky_bball.py
@@ -4,7 +4,6 @@
 
 # Import libraries
 import sys
-import tempfile
 import requests
 from pyspark import StorageLevel
 from pyspark.sql import SparkSession
@@ -25,7 +24,6 @@ def main():
         user=user,
         password=password).load()
 
-    #batter_counts.show()
     # Load 'game' table
     game = spark.read.format("jdbc").options(
         url=f"jdbc:mysql://localhost:{port}/{database}?zeroDateTimeBehavior=convertToNull",
@@ -34,11 +32,30 @@ def main():
         user=user,
         password=password).load()
 
-    #game.show()
-
     # Join tables as "BballCombine"
     BballCombine = game.join(batter_counts, on=['game_id'], how='inner')
 
+    # Assign to temp view
+    batter_counts.createOrReplaceTempView("batter_counts")
+    batter_counts.persist(StorageLevel.DISK_ONLY)
+
+    game.createOrReplaceTempView("game")
+    game.persist(StorageLevel.DISK_ONLY)
+
+    BballCombine.createOrReplaceTempView("BballCombine")
+    BballCombine.persist(StorageLevel.DISK_ONLY)
+
+    # Create rolling table
+    roll = spark.sql(
+        """
+        SELECT a.local_date,a.batter,a.atBat,a.Hit,
+        (SELECT SUM(b.Hit)/NULLIF(SUM(b.atBat),0)
+        FROM BballCombine AS b
+        WHERE DATEDIFF(a.local_date,b.local_date) BETWEEN 0 AND 100 AND a.batter=b.batter) AS battingavg100day
+        FROM BballCombine AS a
+        ORDER BY a.local_date ASC
+        """)
+    roll.show()
 
 if __name__ == "__main__":
     sys.exit(main())
